d5c22574413ab3a5a3e21ec9e4c47e2fbd280ae8
Meolys <elfarissibilal03@gmail.com>
2025-09-14 11:34:48 +0200

    Add files via upload
    Version initial 0.1.0 :
       - Création de l'application Desktop
          - Ajout de ligne de texte explicative
          - Ajout d'un bouton permettant de passer à la page suivante (le bouton ne fonctionne pas encore)

---
+++ Projet Boite a effet personnel/DesktopApp/DesktopApp/DesktopApp/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace DesktopApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.text = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // text
+            // 
+            this.text.AutoSize = true;
+            this.text.Location = new System.Drawing.Point(148, 9);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(478, 13);
+            this.text.TabIndex = 0;
+            this.text.Text = "Bienvenu sur l\'application Desktop du projet realiser à l\'aide du design graphiqu" +
+    "e C# de visual studio";
+            this.text.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(753, 428);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "V0.1.0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(231, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(356, 103);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Cliquer pour entrer";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(288, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(232, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "(note: le bouton ne mene a rien pour le moment)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.text);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label text;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
